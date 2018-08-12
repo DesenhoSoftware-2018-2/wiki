97658cb869f8c81c1c28a0679b1fde59617c8cb3
lucianaribeiro <albuquerqueluciana54@gmail.com>
2018-08-12 09:51:34 -0300

    Setting up new configurations

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from '../components/Home';
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+    </Switch>
+  </BrowserRouter>
+);
