be58f166fccf26e950171347037f259f62742b61
lucianaribeiro <albuquerqueluciana54@gmail.com>
2018-08-12 10:31:55 -0300

    Using md files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
+import test from '../assets/files/test.md'
+const ReactMarkdown = require('react-markdown')
 
 class Home extends Component {
+    constructor(props) {
+        super(props)    
+        this.state = { terms: null }
+    }
+    componentWillMount() {
+        fetch(test).then((r) => r.text()).then((t) => {
+            this.setState({ terms: t })
+        })
+    }
 
     render() {
         return(
             <div>
-                <h1>TESTE</h1>
-                <h2>AAAAAAAAAAAAAA</h2>
+               <ReactMarkdown source={this.state.terms} />
             </div>
         ); 
     }
