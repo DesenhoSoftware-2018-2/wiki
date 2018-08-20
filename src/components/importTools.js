0b0896e8d512f5323bff4cfdda4c02f0f4460726
Indiara Duarte <indiara96d@gmail.com>
2018-08-20 00:02:50 -0300

    Adding tools on homepage

---
+++ src/components/importTools.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class importTools extends Component{
+    render(){
+        const tool = require(`../assets/imgs/tools/${this.props.name}.png`); 
+        return(
+            <div className="tools">
+                    <h4>{this.props.name}</h4>
+                    <img src={tool} alt="tool"/>
+            </div>
+        );
+    }
+}
+
+export default importTools;
