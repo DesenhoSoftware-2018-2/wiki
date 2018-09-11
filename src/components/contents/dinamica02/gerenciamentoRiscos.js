eb2b60266a57a3d50201231cbc6649328a828ef1
Isaque Alves <isaquealvesdl@gmail.com>
2018-09-11 11:17:48 -0300

    #40 Correcting risk management component
    Signed-off-by: Isaque Alves <isaquealvesdl@gmail.com>

--- src/components/contents/dinamica02/gerenciamentoRiscos.js
+++ src/components/contents/dinamica02/gerenciamentoRiscos.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Dinamica01 from './dinamica02';
+import Dinamica02 from './dinamica02';
 import Markdown from '../../structures/dynamicsComponents/markdown';
 
 class GerenciamentoRiscos extends Component {
   render() {
     return (
       <div>
-        <Dinamica01 />
+        <Dinamica02 />
         <div className="container">
           <Markdown file="dinamica02/gerenciamentoRiscos" />
         </div>
