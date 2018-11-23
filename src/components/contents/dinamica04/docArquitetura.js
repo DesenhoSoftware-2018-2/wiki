7c70d7e7d870e0f577328f4571b7cd22c7f2bce0
Isaque Alves <isaquealvesdl@gmail.com>
2018-11-23 01:35:11 -0200

    #63 Update document architecture
    Signed-off-by: Isaque Alves <isaquealvesdl@gmail.com>
    Co-authored: Calebe Rios <calebe_rios@hotmail.com>
    Co-authored-by: Amanda Muniz <amanda.muniiz@outlook.com>
    Co-authored-by: Bruno Dantas <oliveiradantas96@gmail.com>
    Co-authored-by: João Vitor <joaovytor0@gmail.com>

--- src/components/contents/dinamica04/docArquitetura.js
+++ src/components/contents/dinamica04/docArquitetura.js
@@ -7,6 +7,7 @@ import 'react-fancybox/lib/fancybox.css'
 const classev10 = require("../../../assets/imgs/dinamica04/classev10.png");
 const classev20 = require("../../../assets/imgs/dinamica04/classev20.png");
 const classev30 = require("../../../assets/imgs/dinamica04/classev30.png");
+const classev40 = require("../../../assets/imgs/dinamica04/classev40.png");
 const der = require("../../../assets/imgs/dinamica04/der.jpg");
 const logico = require("../../../assets/imgs/dinamica04/logico.jpg");
 
@@ -22,17 +23,26 @@ class DocArquitetura extends Component {
               <h4>Diagrama de Classe</h4>
                <h5>Versão 1.0</h5>
                <ReactFancyBox thumbnail={classev10} image={classev10}/>
+               <a href="https://drive.google.com/open?id=1saGul3uMC1MwvHsWvsVfkmQmEXl9ybcO">Acesse a imagem em tamanho maior aqui</a>
                <p>Autor: Grupo</p>
           </div>
           <div>
                <h5>Versão 2.0</h5>
                <ReactFancyBox thumbnail={classev20} image={classev20}/>
+               <a href="https://drive.google.com/open?id=1Ak8YkBuyNfejZfW8WlxqQ4z8tzHPYa84">Acesse a imagem em tamanho maior aqui</a>
                <p>Autor: Grupo</p>
           </div>
           <div>
                <h5>Versão 3.0</h5>
                <ReactFancyBox thumbnail={classev30} image={classev30}/>
+               <a href="https://drive.google.com/open?id=1PoWpUaeHpIrKpqwxiMmKTri5jJ3vfzV_">Acesse a imagem em tamanho maior aqui</a>
                <p>Autor: Isaque Alves</p>
+          </div>
+          <div>
+               <h5>Versão 4.0</h5>
+               <ReactFancyBox thumbnail={classev40} image={classev40}/>
+               <a href="">Acesse a imagem em tamanho maior aqui</a>
+               <p>Autor: Grupo</p>
           </div>
           <div>
                <h4>Diagrama de Bancos de Dados</h4>
